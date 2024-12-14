52947b234dc6a6b74b1b9c02e22269b35de380e2
lukeamotion <github@lukeamotion.com>
2024-12-14 15:40:12 -0600

    test logentries

--- api/logentries/index.js
+++ api/logentries/index.js
@@ -3,7 +3,9 @@ import { supabase } from '../../utils/supabase';
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     const { data, error } = await supabase.from('logentries').select('*');
-    if (error) return res.status(400).json({ error: error.message });
+    if (error) {
+      return res.status(400).json({ error: error.message });
+    }
     return res.status(200).json(data);
   } else {
     res.setHeader('Allow', ['GET']);
