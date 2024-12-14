62096ee701375fb2b77ea866b8aa0cadb822c7fc
lukeamotion <github@lukeamotion.com>
2024-12-14 15:24:26 -0600

    delete again

--- utils/validation.js
+++
@@ -1,9 +0,0 @@
-export const validateLogEntry = (data) => {
-    if (!data.logType || !['Email', 'Encounter', 'Phone Call', 'Note', 'Meeting', 'Other'].includes(data.logType)) {
-      throw new Error('Invalid logType. Must be one of the predefined types.');
-    }
-    if (!data.logeventtime || isNaN(new Date(data.logeventtime).getTime())) {
-      throw new Error('Invalid logeventtime. Must be a valid date string.');
-    }
-    return true;
-  };
