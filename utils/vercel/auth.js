266c6a277621d6700fbdaeaa7801801eb5ffd03f
lukeamotion <github@lukeamotion.com>
2024-12-20 19:22:34 -0600

    Update auth.js

--- utils/vercel/auth.js
+++ utils/vercel/auth.js
@@ -1,5 +1,8 @@
 export default function authenticate(req, res, next) {
   const apiKey = req.headers['x-api-key'];
+  console.log('Received API Key:', apiKey);
+  console.log('Expected API Key:', process.env.API_SECRET_KEY);
+
   if (apiKey !== process.env.API_SECRET_KEY) {
     return res.status(403).json({ error: 'Unauthorized access. Invalid API key.' });
   }
