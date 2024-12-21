bfff9ae62812a227281ede2bc00e91849618d65b
lukeamotion <github@lukeamotion.com>
2024-12-20 19:05:20 -0600

    Update index.js

--- api/logentries/index.js
+++ api/logentries/index.js
@@ -1,4 +1,4 @@
-import { handleErrors } from '../../utils/vercel/errorHandler'; // Error handling middleware
+import { handleErrors } from '../../utils/vercel/errorhandler'; // Error handling middleware
 import authenticate from '../../utils/vercel/auth'; // Authentication middleware
 import supabase from '../../utils/supabase'; // Supabase client
 
